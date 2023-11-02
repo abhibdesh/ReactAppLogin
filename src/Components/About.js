f9c3cf2785383a2df802f4570fe751af1c29d8a9
Abhilasha Deshmukh <88450158+abhibdesh@users.noreply.github.com>
2023-11-02 18:37:54 +0530

    final

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import download from "../Content/llooggoo.png";
 
 function About() {
   return (
-    <div>
-      skdjfkjsndkjfhksjdhfkjshkdj
-    </div>
-  )
+    <>
+      <div className="container" style={{marginTop:'10px'}}>
+        <div class="card-group">
+          <div class="card">
+            <img src={download} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          <div class="card">
+            <img src={download} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This card has supporting text below as a natural lead-in to
+                additional content.
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          <div class="card">
+            <img src={download} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
